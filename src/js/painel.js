3c32bd38986efa4d1b0c199aab32270eebafc22c
gabrielts212 <91156517+gabrielts212@users.noreply.github.com>
2022-01-21 16:12:51 -0300

    Primeiro commit

---
+++ src/js/painel.js
@@ -0,0 +1,50 @@
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+}
+
+function mostrarImagem() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+/*
+    quando clicar na seta pra avançar temos que 
+    esconder todas as imagens e mostrar a próxima imagem 
+*/
+setaAvancar.addEventListener('click', function() {
+
+    // testa se o contador da imagemAtual é igual ao total de imagens
+    const totalDeImagens = imagensPainel.length - 1;
+    if(imagemAtual === totalDeImagens) {
+        return;
+    }
+
+    imagemAtual++;
+    
+    esconderImagens();
+    mostrarImagem();
+});
+
+/*
+    quando clicar na seta pra voltar temos que 
+    esconder todas as imagens e mostrar a imagem anterior
+*/
+setaVoltar.addEventListener('click', function() {
+
+    if(imagemAtual === 0) {
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagens();
+    mostrarImagem();
+})
+
+  
